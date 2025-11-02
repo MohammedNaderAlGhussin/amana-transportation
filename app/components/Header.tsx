5fa56f950745fa7f97c29cfd70f83945a11de64f
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-02 19:38:09 +0200

    change the header color and styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-gradient-to-r from-green-500 to-green-700 text-white text-center py-10 rounded-b-2xl shadow-md">
+    <header className=" bg-primary-bg text-white text-center py-10  shadow-md">
       <h1 className="text-3xl font-bold">Amana Transportation</h1>
       <p className="text-lg mt-2">
         Proudly Servicing Malaysian Bus Riders Since 2019!
