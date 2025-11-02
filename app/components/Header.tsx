f1c6b1b7ca85cbf30008b3e41410b9347955e344
Mohammed Nader Al Ghussin <92198960+MohammedNaderAlGhussin@users.noreply.github.com>
2025-11-03 00:32:07 +0200

    testing github.dev

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className=" bg-primary-bg text-white text-center py-10  shadow-md">
       <h1 className="text-3xl font-bold">Amana Transportation</h1>
       <p className="text-lg mt-2">
-        Proudly Servicing Malaysian Bus Riders Since 2019!
+        Proudly Servicing Malaysian Bus Riders Since 2019!!
       </p>
     </header>
   );
