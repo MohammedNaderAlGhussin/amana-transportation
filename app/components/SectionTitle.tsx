34d49aaf25653321b4694720b74a8de98421f19b
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-02 19:36:19 +0200

    chang the section title display, color, content and size

--- app/components/SectionTitle.tsx
+++ app/components/SectionTitle.tsx
@@ -1,6 +1,6 @@
 export default function SectionTitle({ title }: { title: string }) {
   return (
-    <div className="bg-yellow-100 text-black font-semibold text-center py-3 text-lg shadow-sm">
+    <div className=" text-[#333333] font-semibold text-start px-[22px] text-2xl  py-4">
       {title}
     </div>
   );
